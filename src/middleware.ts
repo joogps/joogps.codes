6bc8caaf8f93faccc097fb090c821bc5c7e09281
João Gabriel <joogps@gmail.com>
2024-07-24 20:03:48 -0300

    Test

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,23 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
   const pathname = req.nextUrl.pathname;
-  
-  if (pathname.includes('/_next/') || pathname.includes('/static/')) {
-    return NextResponse.next()
+
+  if (pathname.includes("/_next/") || pathname.includes("/static/")) {
+    return NextResponse.next();
   }
 
-  const hostname = req.headers.get('host');
+  const hostname = req.headers.get("host");
   const subdomain = hostname?.match(/^([^.]+)\./)?.[1];
 
   switch (true) {
-    case subdomain?.startsWith('push'):
-      return NextResponse.rewrite(new URL(`/push${req.nextUrl.pathname}`, req.url));
+    case subdomain?.startsWith("push"):
+      return NextResponse.rewrite(
+        new URL(`/push${req.nextUrl.pathname}`, req.url),
+      );
     default:
       return NextResponse.next();
   }
 
-  return NextResponse.next()
+  return NextResponse.next();
 }
