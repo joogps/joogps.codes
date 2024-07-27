a89ccace7708405bc2cd27c7cb04855852cfd890
João Gabriel <joogps@gmail.com>
2024-07-26 23:18:13 -0300

    Add mesh gradient back

--- src/app/push/layout.tsx
+++ src/app/push/layout.tsx
@@ -34,8 +34,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <main className={`text-white bg-black ${inter.className}`}>
+        <main className={`text-white ${inter.className}`}>
           <MeshGradient></MeshGradient>
+          <div className="fixed w-dvw h-dvh bg-black -z-20"></div>
 
           <div className="pt-8 w-full min-h-dvh justify-between flex flex-col">
             <Link href="https://push.joogps.com">
--- src/components/MeshGradient.tsx
+++ src/components/MeshGradient.tsx
@@ -1,15 +1,13 @@
-"use client"
+"use client";
 
 import { useEffect } from "react";
 import { Gradient } from "whatamesh";
 
 export default function MeshGradient() {
-    useEffect(() => {
-      const gradient = new Gradient();
-      gradient.initGradient("#gradient-canvas");
-    }, [])
-  
-    return (
-        <canvas id="gradient-canvas" data-transition-in />
-    );
+  useEffect(() => {
+    const gradient = new Gradient();
+    gradient.initGradient("#gradient-canvas");
+  }, []);
+
+  return <canvas id="gradient-canvas" data-transition-in />;
 }
