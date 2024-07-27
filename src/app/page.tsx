0903f86eaca4e6ef282df7c89739d9b8a0e7415e
João Gabriel <joogps@gmail.com>
2024-07-26 22:36:27 -0300

    Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import ArrowLink from "@/components/ArrowLink";
 
-import DuskLogo from "/public/Dusk.png";
-import GTLogo from "/public/GT.png";
+import DuskLogo from "../../public/Dusk.png";
+import GeorgiaTechLogo from "../../public/Georgia Tech.png";
 
 export default function Home() {
   const year = new Date().getFullYear();
@@ -12,7 +12,7 @@ export default function Home() {
       <div className=" w-full flex flex-col items-center bg-stone-100 dark:bg-black">
         <div className="flex flex-col bg-white dark:bg-zinc-950 p-12 md:my-24 text-2xl font-semibold space-y-8 items-start md:max-w-[660px] md:mx-16 md:rounded-2xl md:border-2 border-stone-200 dark:border-zinc-800">
         <p>
-        <span className="">hey. my name is joão pozzobon</span> — i'm a software engineer from southern Brazil and computer science major at <Image className="inline-block mb-1" src={GTLogo} alt="Dusk" height="22"></Image> <ArrowLink href="https://gatech.edu" className="text-[rgb(106,94,52)] dark:text-[rgb(206,194,152)]">Georgia Tech</ArrowLink>.
+        <span className="">hey. my name is joão pozzobon</span> — i'm a software engineer from southern Brazil and computer science major at <Image className="inline-block mb-1" src={GeorgiaTechLogo} alt="Dusk" height="22"></Image> <ArrowLink href="https://gatech.edu" className="text-[rgb(106,94,52)] dark:text-[rgb(206,194,152)]">Georgia Tech</ArrowLink>.
         </p>
 
         <p>over the years i've launched successful projects such as <span className="opacity-85"><ArrowLink href="https://push.joogps.com" className="expanded-font">push</ArrowLink>, a smart push-up tracking and social workout app</span>, <span className="text-purple-900 dark:text-purple-300"><svg className="inline-block fill-purple-800 dark:fill-purple-400 mb-1" width="22px" height="22px" viewBox="0 0 114.337224 120.124028" version="1.1" xmlns="http://www.w3.org/2000/svg">
