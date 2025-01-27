6ca9136563976b5d889624d80c138f2ae08d5e0a
João Gabriel <joogps@gmail.com>
2025-01-26 19:52:27 -0500

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
   const pathname = req.nextUrl.pathname;
-  
+
   if (pathname.includes("/_next/") || pathname.includes("/static/")) {
     return NextResponse.next();
   }
